1dc473c45dd6016a80ea0e9ca97e3e99cd7bbc84
lzw-723 <2051826135@qq.com>
2020-06-27 11:06:31 +0800

    注册表获取测试

--- extension.js
+++ extension.js
@@ -1,23 +1,31 @@
+/*
+ * @Author: lzw-723
+ * @Date: 2020-03-06 15:28:19
+ * @LastEditors: lzw-723
+ * @LastEditTime: 2020-06-27 11:04:01
+ * @Description: 描述信息
+ * @FilePath: \fxml-viewer\extension.js
+ */
 const vscode = require('vscode');
 const cp = require("child_process");
-
+const regedit = require('regedit');
+const fs = require("fs");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	console.log('Congratulations,extension "fxml-viewer" is now active!');
-
-	let disposable = vscode.commands.registerCommand('extension.openFXML', (event) => {
-		if (getSceneBuilderHome()) {
+	let disposable = vscode.commands.registerCommand('fxml-viewer.openFXML', (event) => {
+		if (getSceneBuilderExecutable() || autoFindPath()) {
 			let fxmlPath = event.path;
-			let sceneBuilderHome = getSceneBuilderHome();
+			let sceneBuilderHome = getSceneBuilderExecutable();
+			console.error(fxmlPath + '----===---' + sceneBuilderHome);
 			viewFXML(fxmlPath, sceneBuilderHome);
-			
 		}
-		else{
-			vscode.window.showErrorMessage('please set ScenceBuilder.Home');
+		else {
+			vscode.window.showErrorMessage('please set SceneBuilder.executable');
+			// console.error(autoFindPath());
+			
 		}
 	});
 
@@ -25,21 +33,66 @@ function activate(context) {
 }
 exports.activate = activate;
 
-function getSceneBuilderHome() {
-	return vscode.workspace.getConfiguration().get('scenebuilder.home');
+function autoFindPath() {
+	let path;
+	if (vscode.workspace.getConfiguration().get('fxml-viewer.autoFindPath')) {
+		console.log('reg');
+		
+		regedit.list(['HKLM\\SOFTWARE\\Gluon\\SceneBuilder']).on('data', function (entry) {
+			let keys01 = entry.data.keys;
+			keys01.forEach(function (key) {
+
+				regedit.list(['HKLM\\SOFTWARE\\Gluon\\SceneBuilder\\' + key]).on('data', function (e) {
+
+					let sum_key = e.key;
+					var keys02 = e.data.keys;
+					if (keys02) {
+
+						keys02.forEach(function (key) {
+
+							if (key == 'ApplicationPath') {
+
+								regedit.list([sum_key + '\\' + key]).on('data', function (e) {
+
+									let val = e.data.values;
+									let builder_path = val[""].value;
+
+									path = builder_path + "\\SceneBuilder.exe";
+									vscode.workspace.getConfiguration().update('fxml-viewer.SceneBuilder.executable', path, true);
+								console.log(path);
+								
+								});
+							}
+						});
+					}
+				});
+			})
+		});
+	}
+}
+
+function getSceneBuilderExecutable() {
+	let executable = vscode.workspace.getConfiguration().get('fxml-viewer.SceneBuilder.executable');
+	if (executable) {
+
+	} else {
+		executable = autoFindPath();
+	}
+	return executable;
 }
 
 function viewFXML(filePath, exePath) {
+
 	cp.execFile(exePath, [filePath], {}, (error, stdout, stderr) => {
 		if (error) {
 			console.log('exec error: ' + error);
-			vscode.window.showErrorMessage("SceneBuilder couldn't opened!");
+			vscode.window.showErrorMessage('SceneBuilder couldn\'t opened!');
 		}
 	});
 }
 
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
